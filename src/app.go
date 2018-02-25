7af12434989061372936012dc755d8e61b312196
liu.renjun <liu.renjun@mydreamplus.com>
2018-02-25 23:21:38 +0800

    流程初始化

--- src/app.go
+++ src/app.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"github.com/parnurzeal/gorequest"
+	"github.com/fatih/color"
+	"strconv"
+	"os"
+)
 
+var (
+	maxPage   = 10
+	startPage = 1
+	Request   *gorequest.SuperAgent
+)
+
+func init() {
+	Request = gorequest.New()
+}
+func main() {
+	
+	for {
+		color.Green("正在获取第" + strconv.Itoa(startPage) + "页数据。")
+		listUrl := makeListUrl(startPage)
+		_, errs := getViewIds(listUrl)
+		if errs != nil {
+			for _, err := range (errs) {
+				color.Red(err.Error())
+			}
+		} else {
+		
+		}
+		startPage = startPage + 1
+		if startPage > maxPage {
+			color.Red("已达到最大页数限制")
+			os.Exit(0)
+		}
+	}
+}
+
+func makeListUrl(page int) string {
+	url := "https://www.douban.com/group/haixiuzu/discussion?start=" + strconv.Itoa((page-1)*25)
+	return url
+}
+func getViewIds(listUrl string) (ids []string, err []error) {
+	ids = make([]string, 25)
+	_, _, errs := Request.Get(listUrl).End()
+	if errs != nil {
+		return nil, errs
+	} else {
+		return ids, nil
+	}
 }
