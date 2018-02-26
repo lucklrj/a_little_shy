23c97670148464d590dbb74253bb8b49c2e53215
Lucklrj <sunny_lrj@yeah.net>
2018-02-26 23:32:13 +0800

    解析显示页面的ID号

--- src/app.go
+++ src/app.go
@@ -5,6 +5,11 @@ import (
 	"github.com/fatih/color"
 	"strconv"
 	"os"
+	"github.com/PuerkitoBio/goquery"
+	"bufio"
+	"strings"
+	"fmt"
+	"regexp"
 )
 
 var (
@@ -21,13 +26,13 @@ func main() {
 	for {
 		color.Green("正在获取第" + strconv.Itoa(startPage) + "页数据。")
 		listUrl := makeListUrl(startPage)
-		_, errs := getViewIds(listUrl)
+		ids, errs := getViewIds(listUrl)
 		if errs != nil {
 			for _, err := range (errs) {
 				color.Red(err.Error())
 			}
 		} else {
-		
+			fmt.Println(len(ids))
 		}
 		startPage = startPage + 1
 		if startPage > maxPage {
@@ -42,11 +47,26 @@ func makeListUrl(page int) string {
 	return url
 }
 func getViewIds(listUrl string) (ids []string, err []error) {
-	ids = make([]string, 25)
-	_, _, errs := Request.Get(listUrl).End()
+	ids = make([]string, 0)
+	_, body, errs := Request.Get(listUrl).End()
 	if errs != nil {
 		return nil, errs
 	} else {
+		//开始提取[ids]
+		doc, err := goquery.NewDocumentFromReader(bufio.NewReader(strings.NewReader(body)))
+		if err !=nil{
+			errs=append(errs, err)
+			return nil,errs
+		}
+		doc.Find(".title").Each(func(i int, s *goquery.Selection) {
+			// For each item found, get the band and title
+			url ,_:= s.Find("a").Attr("href")
+			reg ,_:= regexp.Compile(`.*\/([0-9]+)\/`)
+			match := reg.FindAllStringSubmatch(url, -1)
+			if len(match)==1{
+				ids = append(ids,match[0][1])
+			}
+		})
 		return ids, nil
 	}
 }
